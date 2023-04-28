0b3b8ee635f7cabc1fb567a1e81569a94147de7f
Jeremias91 <126523676+Jeremias91@users.noreply.github.com>
2023-04-28 19:58:03 -0300

    Añado mi archivo de Python a mi branch
    Este código pide en la consola dos números y los almacena en las variables de tipo float "numero1" y "numero2" , para luego usar el método resta que pide dos parámetros, para luego retornar el valor a la variable "resultado" de tipo String, luego mostrar el resultado con un "print".

---
+++ Resta - Jere.py
@@ -0,0 +1,12 @@
+def resta(num1, num2):
+    return num1 - num2
+
+numero1 = float(input("Ingresa el primer número para restar: "))
+numero2 = float(input("Ingresa el segundo número para restar: "))
+
+resultado = resta(numero1, numero2)
+
+print("El resultado de la operación es: ", resultado)
+
+#Tarea de Jeremias
+
