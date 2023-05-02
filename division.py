17a9ce574cca6e78916db5b724d29df472defe7a
Diego Piccinali <130253828+diegopiccinali@users.noreply.github.com>
2023-05-02 07:24:24 -0300

    Agregando división

---
+++ division.py
@@ -0,0 +1,9 @@
+def division():
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    
+    resultado = num1 / num2
+    return resultado
+
+resultado_division = division()
+print("El resultado de la división es:", resultado_division)
