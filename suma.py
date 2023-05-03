5bb80ab5becf18ac919e49a6969bab62a3e5e36f
barbiconi <129999252+barbiconi@users.noreply.github.com>
2023-05-03 17:37:16 -0300

    Delete suma.py

--- suma.py
+++
@@ -1,9 +0,0 @@
-def resta():
-    num1 = float(input("Ingrese el primer valor: "))
-    num2 = float(input("Ingrese el segundo valor: "))
-    
-    resultado = num1 - num2
-    return resultado
-
-resultado_resta = resta()
-print("El resultado de la resta es:", resultado_resta)
