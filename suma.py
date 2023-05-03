c64ba1215e1fae056b333fb2e305b95eff34d4ca
barbiconi <129999252+barbiconi@users.noreply.github.com>
2023-05-03 17:38:18 -0300

    Agregando suma

---
+++ suma.py
@@ -0,0 +1,9 @@
+def suma():
+    num1 = float(input("Ingrese el primer valor: "))
+    num2 = float(input("Ingrese el segundo valor: "))
+    
+    resultado = num1 + num2
+    return resultado
+
+resultado_suma = suma()
+print("El resultado de la suma es:", resultado_suma)
