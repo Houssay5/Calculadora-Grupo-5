ac2ee248a3cc4c4bfd43905a1788435e0a8c4937
Grupo 5 <131920708+Houssay5@users.noreply.github.com>
2023-05-09 21:00:33 -0300

    Update multiplicacion Tobias Tinaro.py

--- multiplicacion Tobias Tinaro.py
+++ multiplicacion Tobias Tinaro.py
@@ -1,8 +1,9 @@
 # Matematica Y Logica Grupo 5 Tobias Tinaro Multiplicacion
 
-numero1 = float(input("Ingrese el primer número a multiplicar: "))
-numero2 = float(input("Ingrese el segundo número a multiplicar: "))
+def multiplicacion():
+    numero1 = float(input("Ingrese el primer número a multiplicar: "))
+    numero2 = float(input("Ingrese el segundo número a multiplicar: "))
+    resultadoMultiplicacion = int(numero1 * numero2)
+    print("El resultado de la multiplicación es:", resultadoMultiplicacion)
 
-resultadoMultiplicacion = int (numero1 * numero2)
-
-print ("El resultado de la multiplicación es:", resultadoMultiplicacion)
+multiplicacion()
